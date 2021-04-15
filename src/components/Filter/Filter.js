f6e9cc2c7703a0872b209968d4191eb1d5d46594
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-15 15:23:22 +0300

    Refactored Filter component with hooks.

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,15 +1,22 @@
-// Imports from React
-import React from 'react';
+// React imports
+import React, { useCallback } from 'react';
+
 // Imports from Redux
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { updateFilter } from '../../redux/contacts/contacts-actions';
 import { getFilterValue } from '../../redux/contacts/contacts-selectors';
-// Imports of helpers
-import PropTypes from 'prop-types';
+
 // Styles imports
 import styles from './Filter.module.css';
 
-const Filter = ({ filterValue, filterUpdate }) => {
+export default function Filter() {
+  const dispatch = useDispatch();
+  const onUpdateFilter = useCallback(
+    event => dispatch(updateFilter(event.target.value)),
+    [dispatch],
+  );
+  const filterValue = useSelector(getFilterValue);
+
   return (
     <div className={styles.filter}>
       <label>
@@ -18,30 +25,11 @@ const Filter = ({ filterValue, filterUpdate }) => {
           type="text"
           name="filter"
           value={filterValue}
-          onChange={filterUpdate}
+          onChange={onUpdateFilter}
           autoComplete="off"
           required
         />
       </label>
     </div>
   );
-};
-
-Filter.defaultProps = {
-  filterValue: '',
-};
-
-Filter.propTypes = {
-  filterValue: PropTypes.string,
-  filterUpdate: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = state => ({
-  filterValue: getFilterValue(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-  filterUpdate: event => dispatch(updateFilter(event.target.value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+}
