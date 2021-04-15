f2c72a103232e027bcaae6c32b094d383c31391c
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-15 15:11:42 +0300

    Refactored UserMenu component with hooks.

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,28 +1,33 @@
 // Imports from React
-import React from 'react';
+import React, { useCallback } from 'react';
+
 // Imports of components
 import Chip from '@material-ui/core/Chip';
 import Avatar from '@material-ui/core/Avatar';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
 // Imports from Redux
-import { connect } from 'react-redux';
-import { getUserName, getUserEmail } from '../../redux/auth/auth-selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUserEmail } from '../../redux/auth/auth-selectors';
 import { logoutUser } from '../../redux/auth/auth-operations';
-// Imports of helpers
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+
 // Styles imports
 import styles from './UserMenu.module.css';
+import { withStyles } from '@material-ui/core/styles';
 
 // Customization of default Chip component from Matarial-UI
 const StyledChip = withStyles({
   root: {
-    'background-color': '#283593',
+    backgroundColor: '#283593',
     color: '#ffffff',
   },
 })(Chip);
 
-const UserMenu = ({ userEmail, onLogout }) => {
+export default function UserMenu() {
+  const dispatch = useDispatch();
+  const onLogout = useCallback(() => dispatch(logoutUser()), [dispatch]);
+  const userEmail = useSelector(getUserEmail);
+
   return (
     <div className={styles.UserMenu}>
       <StyledChip avatar={<Avatar />} label={userEmail} />
@@ -31,24 +36,4 @@ const UserMenu = ({ userEmail, onLogout }) => {
       </button>
     </div>
   );
-};
-
-UserMenu.defaultProps = {
-  userEmail: null,
-};
-
-UserMenu.propTypes = {
-  userEmail: PropTypes.string,
-  onLogout: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = state => ({
-  userName: getUserName(state),
-  userEmail: getUserEmail(state),
-});
-
-const mapDispatchToProps = {
-  onLogout: logoutUser,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
+}
