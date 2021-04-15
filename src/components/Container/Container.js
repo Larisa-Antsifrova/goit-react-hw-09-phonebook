15662f5bfabea09c262ec31983bf5eede1c1eae9
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-15 17:12:04 +0300

    Refactored comments.

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,10 @@
+// React imports
 import React from 'react';
+
+// Helpers imports
 import PropTypes from 'prop-types';
 
+// Styles imports
 import styles from './Container.module.css';
 
 const Container = ({ children }) => (
