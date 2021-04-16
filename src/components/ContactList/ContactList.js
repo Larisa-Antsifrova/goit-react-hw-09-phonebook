ab4f8fb1ea83afe1526769ca48a1f76b95069d3b
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-17 01:19:13 +0300

    Refactored ContactList component with hooks.

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,7 +1,8 @@
-// Imports from React
-import React, { Component } from 'react';
+// React imports
+import React, { useEffect, useCallback } from 'react';
+
 // Imports from Redux
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   fetchContacts,
   deleteContact,
@@ -10,82 +11,66 @@ import {
   getFilteredItems,
   getLoading,
 } from '../../redux/contacts/contacts-selectors';
-// Imports of helpers
-import PropTypes from 'prop-types';
+
+// Helpers imports
 import _ from 'lodash';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
 // Styles imports
 import styles from './ContactList.module.css';
 
-class ContactList extends Component {
-  static propTypes = {
-    filtered: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      }).isRequired,
-    ).isRequired,
-    onDeleteContact: PropTypes.func.isRequired,
+export default function ContactList() {
+  const dispatch = useDispatch();
+
+  // Sending HTTP request to fetch contacts
+  useEffect(() => dispatch(fetchContacts()), [dispatch]);
+
+  const onDeleteContact = useCallback(
+    contactId => dispatch(deleteContact(contactId)),
+    [dispatch],
+  );
+  // Getting data from Redux state through selectors
+  const filtered = useSelector(getFilteredItems);
+  const isLoading = useSelector(getLoading);
+
+  const loaderConfig = {
+    type: 'TailSpin',
+    color: '#80cbc4',
+    height: 50,
+    width: 50,
+    className: styles.loader,
   };
 
-  componentDidMount() {
-    this.props.fetchContacts();
-  }
+  return (
+    <>
+      {isLoading && <Loader {...loaderConfig} />}
 
-  render() {
-    const { filtered, onDeleteContact, isLoading } = this.props;
-    const loaderConfig = {
-      type: 'TailSpin',
-      color: '#80cbc4',
-      height: 50,
-      width: 50,
-      className: styles.loader,
-    };
+      {!_.isEmpty(filtered) && (
+        <ul className={styles.contacts}>
+          {filtered.map(({ id, name, number }) => (
+            <li key={id} className={styles.item}>
+              <div>
+                <p>{name}:</p>
+                <p>{number}</p>
+              </div>
 
-    return (
-      <>
-        {isLoading && <Loader {...loaderConfig} />}
+              <button
+                className={styles.btn}
+                onClick={() => {
+                  onDeleteContact(id);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
 
-        {!_.isEmpty(filtered) && (
-          <ul className={styles.contacts}>
-            {filtered.map(({ id, name, number }) => (
-              <li key={id} className={styles.item}>
-                <div>
-                  <p>{name}:</p>
-                  <p>{number}</p>
-                </div>
-
-                <button
-                  className={styles.btn}
-                  onClick={() => {
-                    onDeleteContact(id);
-                  }}
-                >
-                  Delete
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
-
-        {_.isEmpty(filtered) && !isLoading && (
-          <p className={styles.notification}>No contact found.</p>
-        )}
-      </>
-    );
-  }
+      {_.isEmpty(filtered) && !isLoading && (
+        <p className={styles.notification}>No contact found.</p>
+      )}
+    </>
+  );
 }
-
-const mapStateToProps = state => ({
-  filtered: getFilteredItems(state),
-  isLoading: getLoading(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-  onDeleteContact: contactId => dispatch(deleteContact(contactId)),
-  fetchContacts: () => dispatch(fetchContacts()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
